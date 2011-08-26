4820effccfc86cd9f93ba95d18275d638f1d5476
Jack Zhang <moonboots08@gmail.com>
2011-08-26 00:01:12 -0400

    Regular splits work again

--- tmux_ssh_split_h.sh
+++ tmux_ssh_split_h.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 
 ssh_options=$(tmux showenv | grep "ssh_command" | sed 's/^.*=//')
-#if [ -z "$session_variable_name" ]
-#then
-tmux splitw $1 "exec ssh $ssh_options"
-#else
-  #tmux splitw -h
+if [ -n "$ssh_options" ]
+then
+  tmux splitw $1 "exec ssh $ssh_options"
+else
+  echo "made it to else" > /tmp/temp3
+  tmux splitw $1
   ##exec $SHELL
-#fi
+fi
