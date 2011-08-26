fe91602a2086166b4dbfcac404057e0c62af3b38
Jack Zhang <moonboots08@gmail.com>
2011-08-25 23:50:33 -0400

    Genericizing horizontal vs vertical split

--- tmux_ssh_split_h.sh
+++ tmux_ssh_split_h.sh
@@ -3,7 +3,7 @@
 ssh_options=$(tmux showenv | grep "ssh_command" | sed 's/^.*=//')
 #if [ -z "$session_variable_name" ]
 #then
-tmux splitw -h "exec ssh $ssh_options"
+tmux splitw $1 "exec ssh $ssh_options"
 #else
   #tmux splitw -h
   ##exec $SHELL
